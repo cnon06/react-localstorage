8867fbef1c25ad04ac000355e55ad54e1d361cd3
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-10-08 10:41:59 +0300

    33

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(() =>
+    localStorage.getItem("counter") ? localStorage.getItem("counter") : 0
+  );
+
+  useEffect(() => {
+    // Load the counter value from localStorage when the component mounts
+    const savedCounter = localStorage.getItem("counter");
+    if (savedCounter) {
+      setCounter(parseInt(savedCounter));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Save the counter value to localStorage whenever it changes
+    localStorage.setItem("counter", counter.toString());
+  }, [counter]);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <p>Counter: {counter}</p>
+      <button onClick={incrementCounter}>Increment</button>
+      <button onClick={resetCounter}>Reset</button>
     </div>
   );
 }
